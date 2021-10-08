dbc8655765b35a5cc1f75cb52336a2bed6e51590
superg3m <superg3m@gmail.com>
2021-10-08 13:14:31 -0400

    stuff

--- Assets/Scripts/BulletCollision.cs
+++ Assets/Scripts/BulletCollision.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class Collision : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
